e4cf3922a63cf4060c457d959828e70e58d4ce58
botany111 <55132891+botany111@users.noreply.github.com>
2022-03-14 23:47:36 +0800

    Update Enemy.cs

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -29,7 +29,7 @@ public abstract class Enemy : MonoBehaviour
             if (playerHealth != null)
             {
                 playerHealth.DamagePlayer(damage);
-                //Debug.Log(damage);
+                //Debug.Log(damage );
             }
         }
     }
