232ca8db824e187ff08e5e290d80a9c7b7c285e1
botany111 <55132891+botany111@users.noreply.github.com>
2022-05-22 21:14:53 +0800

    新增背包數據的底層邏輯

---
+++ Assets/Inevntory/InevntoryScripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item ",menuName = "Inventory/new Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemimage;
+    public int itemHeld;
+
+    [TextArea]
+    public string itemInfo;
+
+    public bool equip;
+}
---
+++ Assets/Inevntory/InevntoryScripts/ItemOnWorld.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemOnWorld : MonoBehaviour
+{
+    public Item thisItem;
+    public Inevntory playerInevntory;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            AddNewItem();
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddNewItem()
+    {
+        if (!playerInevntory.itemList.Contains(thisItem))
+        {
+            playerInevntory.itemList.Add(thisItem);
+        }
+        else
+        {
+            thisItem.itemHeld += 1;
+        }
+    }
+}
