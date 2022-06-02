67f07852d1c623e24a5b9c77451e3493069fb47f
botany111 <55132891+botany111@users.noreply.github.com>
2022-06-02 22:51:09 +0800

    重新添加怪物巡邏功能
    增加返回狀態時判斷座標位置讓怪物轉向

--- Assets/Script/Enemy/EnemyWolfAI.cs
+++ Assets/Script/Enemy/EnemyWolfAI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyWolfAI : Enemy
 {
-    public enum Status { idle, walk, /*patrol ,*/ Death , Attack};
+    public enum Status { idle, Chase, Walk , /*patrol ,*/ Death , Attack};
 
     public Status status;
 
@@ -30,6 +30,10 @@ public class EnemyWolfAI : Enemy
 
     public float time;
 
+    private Quaternion targetRotation;
+
+    public float turnSpeed = 0.1f;
+
 
 
     public enum Face { Right, Left }
@@ -39,7 +43,7 @@ public class EnemyWolfAI : Enemy
     {
         base.Start();
 
-        status = Status.idle;
+        status = Status.Walk;
 
         spr = this.transform.GetComponent<SpriteRenderer>();
 
@@ -50,15 +54,9 @@ public class EnemyWolfAI : Enemy
         AttackColl = GetComponent<PolygonCollider2D>();
 
         wait = waitTime;
-        //if (spr.flipX)
-        //{
-        //    face = Face.Left;
-        //}
-        //else
-        //{
-        //    face = Face.Right;
 
-        //}
+        transform.localRotation = Quaternion.Euler(0, 180, 0);
+
         myTransform = this.transform;
         if (GameObject.Find("player") != null)
         {
@@ -81,42 +79,84 @@ public class EnemyWolfAI : Enemy
         switch (status)
         {
             case Status.idle:
+                AttackColl.enabled = false;
+                anim.SetBool("Run", false);
+
                 if (myTransform)
                 {
                     if (Mathf.Abs(myTransform.position.x - PlayerTransform.position.x) < Distance)
                     {
-                        status = Status.walk;
+                        status = Status.Chase;
                     }
                     else
                     {
-                       // status = Status.patrol;
+                         status = Status.Walk;
                     }
                     if (Mathf.Abs(myTransform.position.x - PlayerTransform.position.x) <= 1.2)
                     {
                         status = Status.idle;
                     }
-                    AttackColl.enabled = false;
-                    anim.SetBool("Run", false);
+
 
 
                 }
                 break;
 
-            case Status.walk:
+            case Status.Walk:
+                transform.position = Vector2.MoveTowards(transform.position, moveSpots[i].position, speed * Time.deltaTime);
                 anim.SetBool("Run", true);
 
+                if (Vector2.Distance(transform.position, moveSpots[i].position) < 0.1f)
+                {
+                    if (waitTime <= 0)
+                    {
+                        if (myTransform.position.x == moveSpots[0].position.x)
+                        {
+                            transform.localRotation = Quaternion.Euler(0, 0, 0);
+                        }
+                        else
+                        {
+                            transform.localRotation = Quaternion.Euler(0, 180, 0);
+                        }
+
+                        if (i == 0)
+                        {
+                            i = 1;
+                        }
+                        else
+                        {
+                            i = 0;
+                        }
+
+                        waitTime = wait;
+                    }
+                    else
+                    {
+                        anim.SetBool("Run", false);
+                        waitTime -= Time.deltaTime;
+                    }
+                }
+
+                if (Mathf.Abs(myTransform.position.x - PlayerTransform.position.x) < Distance)
+                {
+                    status = Status.Chase;
+                }
+                break;
+
+            case Status.Chase:
+                anim.SetBool("Run", true);
                 if (PlayerTransform)
                 {
                     if (myTransform.position.x >= PlayerTransform.position.x)
                     {
                         //spr.flipX = true;
-                        transform.localRotation = Quaternion.Euler(0, 0, 0);
+                        transform.localRotation = Quaternion.Euler(0, 180, 0);
                         face = Face.Left;
                     }
                     else
                     {
                         //spr.flipX = false;
-                        transform.localRotation = Quaternion.Euler(0, 180, 0);
+                        transform.localRotation = Quaternion.Euler(0, 0, 0);
                         face = Face.Right;
                     }
                 }
@@ -132,77 +172,21 @@ public class EnemyWolfAI : Enemy
 
                 if (Mathf.Abs(myTransform.position.x - PlayerTransform.position.x) > Distance)
                 {
+                    if (myTransform.position.x == moveSpots[i].position.x)
+                    {
+                        transform.localRotation = Quaternion.Euler(0, 180, 0);
+                    }
+                    else
+                    {
+                        transform.localRotation = Quaternion.Euler(0, 0, 0);
+                    }
                     status = Status.idle;
-                    //if (movingRight == true)
-                    //{
-                    //    spr.flipX = false;
-                    //    //movingRight = true;
-                    //}
-                    //else
-                    //{
-                    //    spr.flipX = true;
-                    //    // movingRight = false;
-                    //}
                 }
                 if (Mathf.Abs(myTransform.position.x - PlayerTransform.position.x) <= 1.3)
                 {
                     status = Status.Attack;
                 }
-
- 
-
-                break;
-
-            //case Status.patrol:
-            //    transform.position = Vector2.MoveTowards(transform.position, moveSpots[i].position, speed * Time.deltaTime);
-            //    anim.SetBool("Run", true);
-
-            //    if (Vector2.Distance(transform.position, moveSpots[i].position) < 0.1f)
-            //    {
-            //        if (waitTime <= 0)
-            //        {
-                        
-            //            if (movingRight == true)
-            //            {
-            //                //spr.flipX = false;
-            //                transform.localRotation = Quaternion.Euler(0, 180, 0);
-            //                movingRight = false;
-            //            }
-            //            else
-            //            {
-            //                //spr.flipX = true;
-            //                transform.localRotation = Quaternion.Euler(0, 0, 0);
-            //                movingRight = true;
-            //            }
-
-            //            if (i == 0)
-            //            {
-
-            //                i = 1;
-            //            }
-            //            else
-            //            {
-
-            //                i = 0;
-            //            }
-
-            //            waitTime = wait;
-            //        }
-            //        else
-            //        {
-            //            anim.SetBool("Run", false);
-            //            waitTime -= Time.deltaTime;
-            //        }
-            //        if (Mathf.Abs(myTransform.position.x - PlayerTransform.position.x) < Distance)
-            //        {
-            //            status = Status.walk;
-            //        }
-
-                    
-            //    }
-
-
-            //    break;
+                break; 
 
             case Status.Death:
                 box2D.enabled = false;
@@ -211,13 +195,7 @@ public class EnemyWolfAI : Enemy
 
             case Status.Attack:
                 anim.SetBool("Attack", true);
-
-                AnimatorStateInfo animatorInfo;
-                animatorInfo = anim.GetCurrentAnimatorStateInfo(0);
-                if ((animatorInfo.normalizedTime > 0.6f) && (animatorInfo.IsName("Wolf_Attack")))//normalizedTime: 範圍0 -- 1,  0是動作開始，1是動作結束
-                {
-                    AttackColl.enabled = true;
-                }
+                Attack();
 
                 if (Mathf.Abs(myTransform.position.x - PlayerTransform.position.x) >= 1.3)
                 {
@@ -228,6 +206,68 @@ public class EnemyWolfAI : Enemy
         }
 
     }
+    void Attack()
+    {
+        AnimatorStateInfo animatorInfo;
+        animatorInfo = anim.GetCurrentAnimatorStateInfo(0);
+        if ((animatorInfo.normalizedTime > 0.6f) && (animatorInfo.IsName("Wolf_Attack")))//normalizedTime: 範圍0 -- 1,  0是動作開始，1是動作結束
+        {
+            AttackColl.enabled = true;
+        }
 
-    
+        
+    }
+
 }
+
+
+//case Status.patrol:
+//    transform.position = Vector2.MoveTowards(transform.position, moveSpots[i].position, speed * Time.deltaTime);
+//    anim.SetBool("Run", true);
+
+//    if (Vector2.Distance(transform.position, moveSpots[i].position) < 0.1f)
+//    {
+//        if (waitTime <= 0)
+//        {
+
+//            if (movingRight == true)
+//            {
+//                //spr.flipX = false;
+//                transform.localRotation = Quaternion.Euler(0, 180, 0);
+//                movingRight = false;
+//            }
+//            else
+//            {
+//                //spr.flipX = true;
+//                transform.localRotation = Quaternion.Euler(0, 0, 0);
+//                movingRight = true;
+//            }
+
+//            if (i == 0)
+//            {
+
+//                i = 1;
+//            }
+//            else
+//            {
+
+//                i = 0;
+//            }
+
+//            waitTime = wait;
+//        }
+//        else
+//        {
+//            anim.SetBool("Run", false);
+//            waitTime -= Time.deltaTime;
+//        }
+//        if (Mathf.Abs(myTransform.position.x - PlayerTransform.position.x) < Distance)
+//        {
+//            status = Status.walk;
+//        }
+
+
+//    }
+
+
+//    break;
