22cd3352e4710aa3461b215a9cc24711763ec845
botany111 <55132891+botany111@users.noreply.github.com>
2022-06-03 18:09:19 +0800

    小怪箭矢 Boss箭矢 腳本

--- Assets/Script/PlayerDefense.cs
+++ Assets/Script/PlayerDefense.cs
@@ -4,38 +4,24 @@ using UnityEngine;
 
 public class PlayerDefense : MonoBehaviour
 {
-    private Animator myAnim;
-
-    public static bool DefenseS;
-
-    private float waitTime;
 
     void Start()
     {
-        myAnim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
-        DefenseS = false;
+
     }
 
     void Update()
     {
-        if(DefenseS == true)
-        {
-            if(waitTime <= 0f)
-            {
-                DefenseS = false;
-                waitTime = 1f;
-            }
-            waitTime -= Time.deltaTime;
-        }
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Arrow"))
         {
-            myAnim.SetTrigger("DefenseS");
-            DefenseS = true;
-
+            Destroy(collision.gameObject);
         }
     }
+
+    
 
 }
