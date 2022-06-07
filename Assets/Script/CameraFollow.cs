48f6f48ba8ba53c8521cd1685303e4eb256cabbc
botany111 <55132891+botany111@users.noreply.github.com>
2022-06-07 17:21:32 +0800

    更改相機的跟隨方式

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -7,29 +7,33 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     public float smoothing;
 
-
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        //GameController.camShake = GameObject.FindGameObjectWithTag("CameraShake").GetComponent<CameraShake>();
     }
 
-    private void LateUpdate()
+    void LateUpdate()
     {
         if (target != null)
         {
             if (transform.position != target.position)
             {
                 Vector3 targetPos = target.position;
+                targetPos.x = Mathf.Clamp(targetPos.x, minPosition.x, maxPosition.x);
+                targetPos.y = Mathf.Clamp(targetPos.y, minPosition.y, maxPosition.y);
                 transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetCamPosLimit(Vector2 minPos, Vector2 maxPos)
     {
-        
+        minPosition = minPos;
+        maxPosition = maxPos;
     }
 }
