027bfbae9ba52831c65399cc3cc9b5f877d768bd
botany111 <55132891+botany111@users.noreply.github.com>
2022-09-23 20:41:09 +0800

    整理資料夾

--- Assets/Inevntory/InevntoryScripts/Inevntory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/new Inventory")]
-public class Inevntory : ScriptableObject
-{
-    public List<Item> itemList = new List<Item>();
-}
