1103cc4f5fbc37101f8d725b600c6969ed5cc27c
botany111 <55132891+botany111@users.noreply.github.com>
2022-09-26 20:26:20 +0800

    優化音效控制

--- Assets/Script/Level03BGM.cs
+++ Assets/Script/Level03BGM.cs
@@ -10,29 +10,44 @@ public class Level03BGM : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<AudioSource>().volume = 0;
         GetComponent<AudioSource>().clip = audios[0];
         GetComponent<AudioSource>().Play();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         FindBoss();
+        BGMfadin();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        GetComponent<AudioSource>().volume = 0;
         GetComponent<AudioSource>().clip = audios[1];
         GetComponent<AudioSource>().Play();
+        GetComponent<BoxCollider2D>().enabled = false;
+
     }
 
     void FindBoss()
     {
         if (!GameObject.Find("Boss01") && Boss == true)
         {
+            GetComponent<AudioSource>().volume = 0;
             GetComponent<AudioSource>().clip = audios[0];
             GetComponent<AudioSource>().Play();
             Boss = false;
+        }
+    }
+
+    void BGMfadin()
+    {
+        if (GetComponent<AudioSource>().volume <= 0.4f)
+        {
+            GetComponent<AudioSource>().volume += 0.1f;
         }
     }
 }
