67d43c050ad1d4b714834b07b501629c65db1e7e
wai1s08 <wai6c06@gmail.com>
2022-09-29 02:04:12 +0800

    04

--- Assets/Script/spike.cs
+++ Assets/Script/spike.cs
@@ -2,27 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spike : MonoBehaviour
+public class spike : Enemy
 {
-        public int damge;
-        private PlayerHealth playerHealth;
+    public int damge;
+    private PlayerHealth playerHealth;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
-        }
-        // Update is called once per frame
-        void Update()
-        {
+    //public static float superTime;
 
-        }
-        void OnTriggerEnter2D(Collider2D other)
+    //public static bool IsSuperTime = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsSuperTime == true)
         {
-            if (other.CompareTag("Player"))
+            superTime -= Time.deltaTime;
+        }
+
+
+        //Debug.Log(superTime);
+
+
+        if (superTime <= 0.5f)
+        {
+            IsSuperTime = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
             {
-                playerHealth.DamagePlayer(damge);
+                if (playerHealth != null)
+                {
+                    playerHealth.DamagePlayer(damge);
+
+                    superTime = playerHealth.SuperTime;
+
+                    IsSuperTime = true;
+                }
             }
         }
-
     }
+
+}
