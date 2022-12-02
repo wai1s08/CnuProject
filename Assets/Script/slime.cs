6397a420674738811d0613a9a78005ee12d118e5
wai1s08 <wai6c06@gmail.com>
2022-12-03 01:36:53 +0800

    追蹤test

--- Assets/Script/slime.cs
+++ Assets/Script/slime.cs
@@ -7,17 +7,28 @@ public class slime : MonoBehaviour
     public float enemyDiedtime1;
     public GameObject enemtDied1;
     public float chasingSpeed;//追逐速度
-  
+    new Vector3 playerPos;
+
+    private void Start()
+    {
+
+    }
     private void OnTriggerEnter2D(Collider2D a)
     {
         Destroy(gameObject, enemyDiedtime1);
     }
-
-
-    void OnDestroy()
+    private void Update()
     {
-        Instantiate(enemtDied1, transform.position, Quaternion.identity);
+        playerPos = GameObject.Find("player").gameObject.transform.position;
+        
+        if (Vector2.Distance(gameObject.transform.position,playerPos) > 5)
+        {
+            gameObject.transform.position = Vector2.MoveTowards(transform.position, playerPos, chasingSpeed * Time.deltaTime);
+
+        }
+
     }
 
-   
+    
+
 }
