3b979161c26a2926ee815cb61c2c33976fdb03f5
wai1s08 <wai6c06@gmail.com>
2022-12-02 22:00:09 +0800

    史萊姆程式test,關卡設計05

---
+++ Assets/Script/slime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slime : MonoBehaviour
+{
+    public float enemyDiedtime1;
+    public GameObject enemtDied1;
+    public float chasingSpeed;//追逐速度
+  
+    private void OnTriggerEnter2D(Collider2D a)
+    {
+        Destroy(gameObject, enemyDiedtime1);
+    }
+
+
+    void OnDestroy()
+    {
+        Instantiate(enemtDied1, transform.position, Quaternion.identity);
+    }
+
+   
+}
