ef5f5cd960f012289e5850550da58afb2473fbd0
botany111 <55132891+botany111@users.noreply.github.com>
2022-12-16 12:53:30 +0800

    新增過場動畫

--- Assets/Script/initPlayer.cs
+++ Assets/Script/initPlayer.cs
@@ -7,25 +7,27 @@ public class initPlayer : MonoBehaviour
 {
     public static int PlayerSpawnScene = 2;
 
-    
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
-       
+
+
 
         if (CheckInit.debugSceneName == null)
         {
             SceneManager.LoadScene(PlayerSpawnScene);
+
         }
         else
         {
             SceneManager.LoadScene(CheckInit.debugSceneName);
             CheckInit.debugSceneName = null;
+
         }
 
-        
+
     }
 
 
