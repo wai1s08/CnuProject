b1450672f90c62149ea13785b3c935c0058acc8f
botany111 <55132891+botany111@users.noreply.github.com>
2022-12-19 21:34:04 +0800

    新增對話系統

---
+++ Assets/Script/Talk system/DailogSystem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DailogSystem : MonoBehaviour
+{
+    [Header("UI")]
+    public Text textLabel;
+    //public Image faceImage;
+
+    [Header("文本文件")]
+    public TextAsset textFile;
+    public int index;
+
+    List<string> textList = new List<string>();
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        GetTextFormFile(textFile);
+    }
+
+    private void OnEnable()
+    {
+        textLabel.text = textList[index];
+        index++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F) && index == textList.Count)
+        {
+            gameObject.SetActive(false);
+            index = 0;
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            textLabel.text = textList[index];
+            index++;
+        }
+    }
+
+    void GetTextFormFile(TextAsset file)
+    {
+        textList.Clear();
+        index = 0;
+
+        var lineDate = file.text.Split('\n');
+
+        foreach(var line in lineDate)
+        {
+            textList.Add(line);
+        }
+    }
+}
---
+++ Assets/Script/Talk system/TalkButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkButton : MonoBehaviour
+{
+    public GameObject Button;
+    public GameObject talkUI;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Button.SetActive(true);
+        Debug.Log("true");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Button.SetActive(false);
+        Debug.Log("false");
+    }
+
+    private void Update()
+    {
+        if (Button.activeSelf && Input.GetKeyDown(KeyCode.F))
+        {
+             talkUI.SetActive(true);
+        }
+    }
+
+}
